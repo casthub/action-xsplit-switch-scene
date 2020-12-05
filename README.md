54eee690a554877438792357e74eb5c6dc714d9a
OYED <tom@mosey.io>
2020-12-05 18:44:40 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# CastHub Automations Card (Action) - XSplit Switch Scene
+
+This is the source for the CastHub XSplit Switch Scene Automation Card.
---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = class extends window.casthub.card.action {
+
+    /**
+     * @return {Promise}
+     */
+    async mounted() {
+        const { id } = this.identity;
+
+        // Open the WebSocket Connection to XSplit Broadcaster.
+        this.ws = await window.casthub.ws(id);
+
+        await super.mounted();
+    }
+
+    /**
+     * @param {Object} input
+     */
+    async run(input) {
+        this.ws.send('setActiveScene', {
+            id: this.props.scene,
+        });
+    }
+
+    /**
+     * @return {Promise}
+     */
+    async prepareProps() {
+        let def = '';
+        const raw = this.ws.send('getAllScenes');
+        const scenes = raw.reduce((obj, scene) => {
+            obj[scene.id] = {
+                name: scene.name,
+            };
+
+            return obj;
+        }, {});
+
+        return {
+            scene: {
+                type: 'select',
+                label: 'Scene',
+                default: def,
+                options: scenes,
+            },
+        };
+    }
+
+};
